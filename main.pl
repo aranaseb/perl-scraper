ea73b0b6ecfed804f02c6efa1833bb396706340c
Sebastian Arana <aranaseb@arizona.edu>
2025-07-19 21:09:38 -0700

    Added normalization

--- main.pl
+++ main.pl
@@ -13,8 +13,9 @@
 use strict;
 use warnings;
 use constant DEBUG => 0;
+$/ = "";  # Multi-line paragraph mode for file reading
 
-sub getFiles {
+sub get_files {
 	my @input_files = @_;
 	my @files = ();
 	while (@ARGV) {
@@ -34,23 +35,49 @@ sub trim {
 	return $s;
 }
 
-sub main {
-	my @files = getFiles @ARGV;
-	foreach my $file (@files) {
-		open (my $FILE, "<", $file) or die "$!";
-		while (my $line = <$FILE>) {
-			my @text = "";
-			
-			print $line if DEBUG;
-			
-			if ($line =~ /[ \t]+</) {
-				(@text) = split /<.*?>/, trim $line;
-			}
+sub process_line {
+	print if DEBUG;
+	my @text;
 
-			@text = grep { $_ ne '[ \t\n]+' } @text;
-			print join('',@text),"\n" if @text;
-		}
-		close $FILE;
+	# operates on $_
+	if (/[ \t]+</) {
+		(@text) = split /<.*?>/, trim $_;
+	}
+
+	# remove space only lines
+	@text = grep { $_ ne '[ \t\n]+' } @text;
+
+	# remove embedded tags
+	@text = grep { $_ !~ /<.*(\/>|)/ } @text;
+	return @text;
+}
+
+sub process_file {
+	my $filename = shift;
+	my $text = "";
+	open (FILE, $filename) || die "Can't open file: $!\n";
+	while (<FILE>) {
+		my @chunk = process_line;
+		$text .= join('',@chunk);
+	}
+	return $text;
+}
+
+sub normalize {
+	my $s = shift;
+    $s =~ s/[\,\:\(\)]+//g;	# remove punctuation
+	$s =~ s/\.(?=\s)//g;	# remove periods (at ends of words)
+	$s =~ s/&.*?;//g;	# remove '&rquo;' type characters
+	$s =~ s/\s+/ /g;	# remove spaces
+	return $s;	
+}
+
+sub main {
+	my @files = get_files @ARGV;
+	foreach my $file (@files) {
+		my $text = process_file $file;
+		$text = normalize $text;
+		print $text, "\n";
 	}
 }
 
